daf1ccbac435a3ceeeaf329d03086fd75ef52815
entryone <a.i.kulinich@gmail.com>
2022-06-19 11:01:44 +0300

    Observe parent height

--- src/ReGrid.js
+++ src/ReGrid.js
@@ -1,28 +1,32 @@
-import React, {useState, useRef, useMemo} from "react";
-import "./style.css";
-import { onScroll } from './scroll/scroll'
-import { ScrollBar } from './scroll/ScrollBar'
-
+import React, { useState, useEffect, useRef } from 'react';
+import './style.css';
+import { onScroll } from './scroll/scroll';
+import { ScrollBar } from './scroll/ScrollBar';
 
 export function ReGrid() {
-  const [barY, setBarY] = useState(0)
-  const height = 500
-  const contentHeight = 5000
-  const barHandlerHeight = 30
-  const scrollBarMaxY = height - barHandlerHeight
-  const wheelScrollFactor = height/contentHeight
-  const onWheel = e => {
-    setBarY(onScroll(e, barY, e.deltaY, 0, scrollBarMaxY, wheelScrollFactor))
-  }
-  const onBarMove = e => {
-    setBarY(onScroll(e, barY, e.deltaY, 0, scrollBarMaxY))
-  }
-
-  const contentY = barY / wheelScrollFactor
+  const [height, setHeight] = useState(0);
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current &&
+      new ResizeObserver(() => {
+        setHeight(ref.current.parentElement.offsetHeight);
+      }).observe(ref.current);
+  }, [ref.current]);
+  const [barY, setBarY] = useState(0);
+  const contentHeight = 5000;
+  const barHandlerHeight = 30;
+  const scrollBarMaxY = height - barHandlerHeight;
+  const wheelScrollFactor = height / contentHeight;
+  const onWheel = (e) => {
+    setBarY(onScroll(e, barY, e.deltaY, 0, scrollBarMaxY, wheelScrollFactor));
+  };
+  const onBarMove = (e) => {
+    setBarY(onScroll(e, barY, e.deltaY, 0, scrollBarMaxY));
+  };
+  const contentY = barY / wheelScrollFactor;
   return (
-    <div onWheel={onWheel}  style={{width: '100%', height: height}}>
-      barY = {barY} |
-      contentY = {contentY}
+    <div ref={ref} onWheel={onWheel} style={{ width: '100%', height: height }}>
+      barY = {barY} | contentY = {contentY}
       <ScrollBar onMove={onBarMove} x={0} y={barY} />
     </div>
   );
